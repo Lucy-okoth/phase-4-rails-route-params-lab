a96ecf84c00eeaec00516158880a7dea4c8b88cd
Lucy okoth <lucyakokoth@gmail.com>
2022-11-26 01:09:26 +0300

    Completed lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
+    #students = if params[:name]
+    #  Student.find_by(:name params[:name])
+    #else
     students = Student.all
+    #end
     render json: students
   end
 
+  def show
+    students = Student.find(params[:id])
+    render json: students
+  end
 end
